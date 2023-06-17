fcbf7d6fb7ef987a750a64e3b21a7e13e730854d
ZhaoCan <1725101736@qq.com>
2023-06-17 21:28:40 +0800

    upload uniquePtr

---
+++ Engine/Core/SmartPointer/UniquePtr.h
@@ -0,0 +1,47 @@
+#pragma once
+
+namespace Coldlight
+{
+	template <typename T>
+	class CUniquePtr 
+	{
+	public:
+		// Default Constructor
+		CUniquePtr()
+		{
+			m_Data = new T();
+		}
+
+		CUniquePtr(CUniquePtr& inPtr)
+		{
+			m_Data = inPtr.m_Data;
+			inPtr.m_Data = nullptr;
+		}
+
+		// Copy Assignment
+		CUniquePtr& operator=(CUniquePtr& inPtr)
+		{
+			if (this == &inPtr)
+			{
+				return *this;
+			}
+
+			m_Data = inPtr.m_Data;
+			inPtr.m_Data = nullptr;
+			return *this;
+		}
+
+		~CUniquePtr() 
+		{
+			delete m_Data;
+		}
+
+		__forceinline T* Get() const
+		{
+			return m_Data;
+		}
+
+	private:
+		T* m_Data = nullptr;
+	};
+}
