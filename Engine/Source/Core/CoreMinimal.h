4b4a432fd94aca95ff6228053f6a48ffa9b67592
Sihao Zhang <shikou3d@gmail.com>
2025-04-16 02:23:03 +0800

    build: resolve Linux build failure

--- Engine/Source/Core/CoreMinimal.h
+++ Engine/Source/Core/CoreMinimal.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #if CL_PLATFORM_WINDOWS
-    #include <Windows.h>
     #ifdef CL_CORE_API_EXPORTS
         #define CORE_API __declspec(dllexport)
     #else
         #define CORE_API __declspec(dllimport)
     #endif
 #elif CL_PLATFORM_APPLE || CL_PLATFORM_LINUX
-    #define CORE_API
+    #ifdef CL_CORE_API_EXPORTS
+        #define CORE_API __attribute__((visibility("default")))
+    #else
+        #define CORE_API
+    #endif
 #else
     #error "Coldlight Engine only supports Windows, macOS, and Linux!"
 #endif
